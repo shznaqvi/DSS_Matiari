0d412bb1a6f7fa0896b38735ba8fecae1418a2ec
shznaqvi <shznaqvi@gmail.com>
2021-07-08 18:41:03 +0500

    working..

---
+++ app/src/main/java/edu/aku/hassannaqvi/dss_matiari/ui/sections/IdentificationActivity.java
@@ -0,0 +1,16 @@
+package edu.aku.hassannaqvi.dss_matiari.ui.sections;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import edu.aku.hassannaqvi.dss_matiari.R;
+
+public class IdentificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_identification);
+    }
+}
