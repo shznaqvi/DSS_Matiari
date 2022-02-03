d5bc6a375ee4d02664bf172c55fd5d98682ad66e
shznaqvi <shznaqvi@gmail.com>
2022-02-03 18:25:20 +0500

    Follow-UPS-X

--- app/src/main/java/edu/aku/hassannaqvi/dss_matiari/core/MainApp.java
+++ app/src/main/java/edu/aku/hassannaqvi/dss_matiari/core/MainApp.java
@@ -39,8 +39,8 @@ public class MainApp extends Application {
     // public static final String _IP = "https://cls-pae-fp51764";// .TEST server
     //public static final String _IP = "http://43.245.131.159:8080";// .TEST server
     public static final String _HOST_URL = MainApp._IP + "/dss_matiari/api/";// .TEST server;
-    public static final String _SERVER_URL = "sync.php";
-    public static final String _SERVER_GET_URL = "getData.php";
+    public static final String _SERVER_URL = "syncenc.php";
+    public static final String _SERVER_GET_URL = "getDataEnc.php";
     public static final String _PHOTO_UPLOAD_URL = _HOST_URL + "uploads.php";
     public static final String _UPDATE_URL = MainApp._IP + "/dss_matiari/app/";
 
