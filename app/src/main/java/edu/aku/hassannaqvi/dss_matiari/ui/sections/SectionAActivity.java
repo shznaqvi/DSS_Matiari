8530ebd92001cc5d235182b7dfec5e3851e85bf1
gul.sanober <gulsanobermemon@gmail.com>
2023-05-09 13:40:56 +0500

    implemented roaster changes

--- app/src/main/java/edu/aku/hassannaqvi/dss_matiari/ui/sections/SectionAActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/dss_matiari/ui/sections/SectionAActivity.java
@@ -57,35 +57,7 @@ public class SectionAActivity extends AppCompatActivity {
         if (households.getUid().equals(""))
         {
             households.populateMeta();
-            bi.btnContinue.setVisibility(View.VISIBLE);
-        }else{
-            /*if(households.getRegRound().equals("")) {
-                bi.btnContinue.setVisibility(View.GONE);
-                bi.btnUpdate.setVisibility(View.VISIBLE);
-                households.setRa08(MainApp.hhsList.get(MainApp.selectedFpHousehold).getRa08());
-                households.setRa12(MainApp.hhsList.get(MainApp.selectedFpHousehold).getRa12());
-                households.setRa17_a1(MainApp.hhsList.get(MainApp.selectedFpHousehold).getRa17_a1().equals("null") ? "0" : MainApp.hhsList.get(MainApp.selectedHousehold).getRa17_a1());
-                households.setRa17_b1(MainApp.hhsList.get(MainApp.selectedFpHousehold).getRa17_b1().equals("null") ? "0" : MainApp.hhsList.get(MainApp.selectedHousehold).getRa17_b1());
-                households.setRa17_c1(MainApp.hhsList.get(MainApp.selectedFpHousehold).getRa17_c1().equals("null") ? "0" : MainApp.hhsList.get(MainApp.selectedHousehold).getRa17_c1());
-                households.setRa17_d1(MainApp.hhsList.get(MainApp.selectedFpHousehold).getRa17_d1().equals("null") ? "0" : MainApp.hhsList.get(MainApp.selectedHousehold).getRa17_d1());
-                households.setRa17_a2(MainApp.hhsList.get(MainApp.selectedFpHousehold).getRa17_a2().equals("null") ? "0" : MainApp.hhsList.get(MainApp.selectedHousehold).getRa17_a2());
-                households.setRa17_b2(MainApp.hhsList.get(MainApp.selectedFpHousehold).getRa17_b2().equals("null") ? "0" : MainApp.hhsList.get(MainApp.selectedHousehold).getRa17_b2());
-                households.setRa17_c2(MainApp.hhsList.get(MainApp.selectedFpHousehold).getRa17_c2().equals("null") ? "0" : MainApp.hhsList.get(MainApp.selectedHousehold).getRa17_c2());
-                households.setRa17_d2(MainApp.hhsList.get(MainApp.selectedFpHousehold).getRa17_d2().equals("null") ? "0" : MainApp.hhsList.get(MainApp.selectedHousehold).getRa17_d2());
-                households.setRa17_a3(MainApp.hhsList.get(MainApp.selectedFpHousehold).getRa17_a3().equals("null") ? "0" : MainApp.hhsList.get(MainApp.selectedHousehold).getRa17_a3());
-                households.setRa17_b3(MainApp.hhsList.get(MainApp.selectedFpHousehold).getRa17_b3().equals("null") ? "0" : MainApp.hhsList.get(MainApp.selectedHousehold).getRa17_b3());
-                households.setRa17_c3(MainApp.hhsList.get(MainApp.selectedFpHousehold).getRa17_c3().equals("null") ? "0" : MainApp.hhsList.get(MainApp.selectedHousehold).getRa17_c3());
-                households.setRa17_d3(MainApp.hhsList.get(MainApp.selectedFpHousehold).getRa17_d3().equals("null") ? "0" : MainApp.hhsList.get(MainApp.selectedHousehold).getRa17_d3());
-                households.setRa18(MainApp.hhsList.get(MainApp.selectedFpHousehold).getRa18());
-                if (Integer.parseInt(households.getRa18()) > 0) {
-                    households.setRa15("1");
-                } else {
-                    households.setRa15("2");
-                }
-            }*/
-
         }
-
         bi.setHousehold(households);
 
         setTitle(R.string.demographicinformation_mainheading);
@@ -130,22 +102,6 @@ public class SectionAActivity extends AppCompatActivity {
 
     }
 
-    public void btnUpdateHH(View view){
-
-        if (!formValidation()) return;
-        if (!insertNewRecord()) return;
-
-        if (updateDB()) {
-
-            setResult(RESULT_OK);
-            Intent intent = new Intent(this, FPMwraActivity.class);
-            ((Activity) this).startActivityForResult(intent, 2);
-
-        } else {
-            Toast.makeText(this, "Failed to Update Database!", Toast.LENGTH_SHORT).show();
-        }
-
-    }
 
 
 
