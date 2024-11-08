b889c4929352393c1cc397c43fbd6dd5f83a74ab
Muhammad Hussain <ahsanpervez74@gmail.com>
2024-11-08 18:37:22 +0500

    minor coorection in SyncFunctionsDao.kt

--- app/src/main/java/edu/aku/hassannaqvi/dss_matiari/database/dao/SyncFunctionsDao.kt
+++ app/src/main/java/edu/aku/hassannaqvi/dss_matiari/database/dao/SyncFunctionsDao.kt
@@ -23,24 +23,30 @@ interface SyncFunctionsDao {
 
     /**************** MWRA ********************/
 
-    @Query("SELECT * FROM MWRAs WHERE (synced is '' OR synced is NULL) AND (istatus != 4) ORDER BY _id ASC ")
+    @Query("SELECT * FROM MWRAs WHERE (synced is '' OR synced is NULL) AND (istatus != 4 and istatus != '') ORDER BY _id ASC ")
     fun getUnsyncedMWRAS_internal(): List<Mwra>
 
-    @Query("SELECT child.* FROM MWRAs child LEFT JOIN hhs parent ON child._uuid = parent._uid WHERE ((child._uuid IN (:uuid)) OR (child.synced = '' AND parent.synced != '')) AND child.istatus = 1 OR parent.visitNo > 2 OR child.isError IS 1 ORDER BY _id ASC")
-    fun getAllUnSyncedMWRAsByUIds(uuid: List<String?>?): List<Mwra?>?
+    /*@Query("SELECT child.* FROM MWRAs child LEFT JOIN hhs parent ON child._uuid = parent._uid WHERE ((child._uuid IN (:uuid)) OR (child.synced = '' AND parent.synced != '')) AND child.istatus = 1 OR parent.visitNo > 2 OR child.isError IS 1 ORDER BY _id ASC")
+    fun getAllUnSyncedMWRAsByUIds(uuid: List<String?>?): List<Mwra?>?*/
+
+    @Query("SELECT child.* FROM MWRAs child LEFT JOIN hhs parent ON child._uuid = parent._uid WHERE ((child._uuid IN (:uuid) OR (child.synced = '' AND parent.synced != '')) AND child.istatus != 4 AND child.istatus != '') OR child.isError IS 1 ORDER BY _id ASC")
+    fun getAllUnSyncedMWRAsByUIds(uuid: List<String?>?): List<Mwra>
 
     @Query("SELECT child.* FROM outcomes child LEFT JOIN hhs parent ON child._uuid = parent._uid WHERE ((child._uuid IN (:uuid)) OR (child.synced = '' AND parent.synced != '')) AND child.istatus = 1 OR parent.visitNo > 2 OR child.isError IS 1 ORDER BY _id ASC")
     fun getAllUnSyncedOutcomesByUIds(uuid: List<String?>?): List<Mwra?>?
 
     fun getUnsycedMWRAS(): List<Mwra> {
         val hhsSync = getUnsyncedHousehols()
-        val allMwras = getUnsyncedMWRAS_internal()
-        val toSyncMwras = arrayListOf<Mwra>()
+        //        val allMwras = getUnsyncedMWRAS_internal()
+        val uidList = hhsSync.map { it.uid }
+        val allMwras = getAllUnSyncedMWRAsByUIds(uidList)
+        /*val toSyncMwras = arrayListOf<Mwra>()
         hhsSync.forEach { hhs ->
-            val mwras = allMwras.filter { it.hdssId == hhs.hdssId && it.uuid == hhs.uid }
+            val mwras = allMwras.filter { (it.hdssId == hhs.hdssId && it.uuid == hhs.uid) || (it.synced.isEmpty()) }
             toSyncMwras.addAll(mwras)
         }
-        return toSyncMwras
+        return toSyncMwras*/
+        return allMwras
     }
 
     /**************** Outcome ********************/
